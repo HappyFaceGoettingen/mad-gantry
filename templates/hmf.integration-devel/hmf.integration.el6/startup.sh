f76f62841477df75124f5a86c76ab0c9b05d9800
GenKawamura <gen.kawamura@cern.ch>
2018-02-28 17:56:35 +0100

    Added configs dir

--- templates/hmf.integration-devel/hmf.integration.el6/startup.sh
+++ templates/hmf.integration-devel/hmf.integration.el6/startup.sh
@@ -17,6 +17,35 @@ if [ ! -d /home/${USER} ] ; then
   echo "${USER}:${PASSWORD}" |chpasswd
 fi
 
+
+## --------------------------------------
+##  MadMask development env
+## --------------------------------------
+
+## Changing ownership of Docker volume dirs to the HappyFace user.
+chown -R happyface3:happyface3 /devel /sites /var/lib/MadMaskData
+
+## Installing MadMask
+pushd /devel/HappyFace-MadMask
+./rebuild.sh build madmask
+./rebuild.sh build madfoxd
+#./rebuild.sh build libs
+./rebuild.sh test
+
+## Changing directories
+MADMASK_HOME=/var/lib/HappyFace3/MadMask
+[ ! -L  $MADMASK_HOME/sites ] && mv -v $MADMASK_HOME/sites $MADMASK_HOME/sites.org
+ln -vs /sites $MADMASK_HOME/sites
+popd
+
+## Starting MadFoxd & MadMaskd
+service madfoxd start
+service madmaskd start
+
+##  Done!! 
+
+
+## Starting X and ssh
 /usr/bin/xdm
 service xinetd start
 # Start the ssh service
