9a4948e8fbe22326995410a7b89ae9bc97048a76
GenKawamura <gen.kawamura@cern.ch>
2018-03-02 16:33:14 +0100

    Added some other sites in DE and gunship method

--- libs/ssh.sh
+++ libs/ssh.sh
@@ -10,14 +10,14 @@ generate_sshkey(){
 connect_via_ssh(){
     local site_name=$1
     [ ! -e $PAYLOADS_DIR/ssh-key/rsa ] && echo "$PAYLOADS_DIR/ssh-key/rsa does not exist" && return 1 
-    [ -z "$site_name" ] && echo "[$site_name] is empty" && return 1
+    [ -z "$site_name" ] && echo "site_name [$site_name] is empty" && return 1
 
     local ssh_port=$(get_ssh_port $site_name)
     [ -z "$ssh_port" ] && echo "Cannot find ssh_port [$ssh_port]" && return 1 
     local ssh_host=localhost
     echo "Connecting to $site_name container [$ssh_host:$ssh_port] ..."
-    echo "ssh -Y -o StrictHostKeyChecking=no -i $PAYLOADS_DIR/ssh-key/rsa -p $ssh_port root@$ssh_host"
-    ssh -Y -o StrictHostKeyChecking=no -i $PAYLOADS_DIR/ssh-key/rsa -p $ssh_port root@$ssh_host
+    echo "ssh -Y -o StrictHostKeyChecking=no -o PasswordAuthentication=no -i $PAYLOADS_DIR/ssh-key/rsa -p $ssh_port root@$ssh_host"
+    ssh -Y -o StrictHostKeyChecking=no -o PasswordAuthentication=no -i $PAYLOADS_DIR/ssh-key/rsa -p $ssh_port root@$ssh_host
     return $?
 }
 
