276ad271e8b1202127ce6ff2e7b9be3c898da316
GenKawamura <gen.kawamura@cern.ch>
2018-03-17 20:40:33 +0100

    Updated build-madmask.sh

--- templates/hmf.integration-devel/hmf.integration.el6/build-madmask.sh
+++ templates/hmf.integration-devel/hmf.integration.el6/build-madmask.sh
@@ -11,25 +11,29 @@ chown -R root:root /root/.ssh
 
 ## Building and Installing HappyFace-MadMask
 pushd /devel/HappyFace-MadMask
-./rebuild.sh -w /tmp/MadMask -b madmask
-./rebuild.sh -w /tmp/MadMask -b madfoxd
-./rebuild.sh -w /tmp/MadMask -b rlibs
-./rebuild.sh -w /tmp/MadMask -t
+./rebuild.sh -b madmask
+./rebuild.sh -b madfoxd
+./rebuild.sh -b rlibs
+./rebuild.sh -t
 popd
+
 
 ## Changing configurations in HappyFace MadModules
 
 
 ## Changing Site directory in MadMask
-MADMASK_HOME=/var/lib/HappyFace3/MadMask
-[ ! -L  $MADMASK_HOME/sites ] && mv -v $MADMASK_HOME/sites $MADMASK_HOME/sites.org
 SITE_NAME=$(ls /sites | grep -v default | head -n 1)
 echo "Site = [$SITE_NAME]"
 [ ! -e /sites/default ] && ln -vs $SITE_NAME /sites/default
+
+MADMASK_HOME=/var/lib/HappyFace3/MadMask
+echo "Applying a site configuration to [$MADMASK_HOME] in this container ..."
+[ ! -L  $MADMASK_HOME/sites ] && mv -v $MADMASK_HOME/sites $MADMASK_HOME/sites.org
 ln -vs /sites $MADMASK_HOME/sites
 
 
 ## Starting MadFoxd & MadMaskd (Ionic Mobile Server)
+echo "Starting MadMask and MadFox daemons"
 service madmaskd start
 service madfoxd start
 
