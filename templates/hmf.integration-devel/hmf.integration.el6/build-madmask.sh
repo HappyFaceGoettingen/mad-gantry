32c0bf1ad7b6579ba055dbccfffdc2107c05b324
GenKawamura <gen.kawamura@cern.ch>
2018-03-17 13:28:25 +0100

    Updated hmf.integration build script

--- templates/hmf.integration-devel/hmf.integration.el6/build-madmask.sh
+++ templates/hmf.integration-devel/hmf.integration.el6/build-madmask.sh
@@ -5,8 +5,9 @@
 ## --------------------------------------
 
 ## Changing ownership of Docker volume dirs to the HappyFace user.
-chown -R happyface3:happyface3 /devel /sites /var/lib/MadMaskData
+chown -R happyface3:happyface3 /var/lib/MadMaskData
 chown -R root:root /root/.ssh 
+
 
 ## Building and Installing HappyFace-MadMask
 pushd /devel/HappyFace-MadMask
@@ -16,9 +17,15 @@ pushd /devel/HappyFace-MadMask
 ./rebuild.sh -w /tmp/MadMask -t
 popd
 
-## Changing directories
+## Changing configurations in HappyFace MadModules
+
+
+## Changing Site directory in MadMask
 MADMASK_HOME=/var/lib/HappyFace3/MadMask
 [ ! -L  $MADMASK_HOME/sites ] && mv -v $MADMASK_HOME/sites $MADMASK_HOME/sites.org
+SITE_NAME=$(ls /sites | grep -v default | head -n 1)
+echo "Site = [$SITE_NAME]"
+[ ! -e /sites/default ] && ln -vs $SITE_NAME /sites/default
 ln -vs /sites $MADMASK_HOME/sites
 
 
