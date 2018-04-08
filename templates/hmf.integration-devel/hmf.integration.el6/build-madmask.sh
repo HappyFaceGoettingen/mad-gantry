2e6ea2f72433ea9ad686d1b9c3b14df6c33b2d4a
GenKawamura <gen.kawamura@cern.ch>
2018-04-08 22:13:12 +0200

    Added proper DE template and Android SDK tool

--- templates/hmf.integration-devel/hmf.integration.el6/build-madmask.sh
+++ templates/hmf.integration-devel/hmf.integration.el6/build-madmask.sh
@@ -8,35 +8,50 @@
 chown -R happyface3:happyface3 /var/lib/MadMaskData
 chown -R root:root /root/.ssh 
 
+
+## Building and Installing HappyFace-MadMask
+pushd /devel/HappyFace-MadMask
+./rebuild.sh -p
+./rebuild.sh -b madmask
+./rebuild.sh -b madmodules
+./rebuild.sh -b madfoxd
+./rebuild.sh -b rlibs
+./rebuild.sh -b android-sdk
+./rebuild.sh -t
+popd
+
+
 ## Getting the site name
 SITE_NAME=$(ls /sites | grep -v default | head -n 1)
 echo "Site = [$SITE_NAME]"
 
-## Building and Installing HappyFace-MadMask
-pushd /devel/HappyFace-MadMask
-./rebuild.sh -b madmask
-./rebuild.sh -b madmodules
-./rebuild.sh -b madfoxd
-[ "$SITE_NAME" == "ADC" ] && ./rebuild.sh -b android-sdk
-./rebuild.sh -t
-popd
-
-## Changing ownership of Android SDK and updating it
-if [ "$SITE_NAME" == "ADC" ]; then
-    chown -R happyface3:happyface3 /usr/local/android-tools
-    #su - happyface3 -c ". /etc/profile; update-android-sdk"
-fi
-
-## Changing configurations in HappyFace MadModules
-
-
 ## Changing Site directory in MadMask
 [ ! -e /sites/default ] && ln -vs $SITE_NAME /sites/default
-
 MADMASK_HOME=/var/lib/HappyFace3/MadMask
 echo "Applying a site configuration to [$MADMASK_HOME] in this container ..."
 [ ! -L  $MADMASK_HOME/sites ] && mv -v $MADMASK_HOME/sites $MADMASK_HOME/sites.org
 ln -vs /sites $MADMASK_HOME/sites
+
+
+## Building the Android application
+echo "Building the Android APKs ..."
+rsync -avlogp /usr/local/android-tools/.android $MADMASK_HOME/
+
+## Changing ownership of Android SDK and updating it
+if [ "$SITE_NAME" == "ADC" ]; then
+    chown -R happyface3:happyface3 /usr/local/android-tools
+    su - happyface3 -c ". /etc/profile; update-android-sdk"
+    rsync -avlogp --delete $MADMASK_HOME/.android /usr/local/android-tools/
+fi
+
+su - happyface3 -c ". /etc/profile; $MADMASK_HOME/madmask -b 2>&1 | tee /tmp/madmask.android-build.log"
+
+
+
+
+## Changing configurations in HappyFace MadModules
+
+
 
 
 ## Starting MadFoxd & MadMaskd (Ionic Mobile Server)
