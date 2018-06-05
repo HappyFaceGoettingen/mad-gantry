90c6b55e9531fd187710d3ee8ec5f41caa4d75cb
GenKawamura <gen.kawamura@cern.ch>
2018-06-06 00:55:58 +0200

    Added a routine to change ionic/mobile/web ports

--- libs/custom_ship_configs.sh
+++ libs/custom_ship_configs.sh
@@ -1,5 +1,6 @@
 apply_custom_ship_configs(){
-    local config_json=$1
+    local site=$1
+    local config_json=$2
     [ ! -e $config_json ] && echo "[$config_json] does not exist" && return 1
 
     echo "Applying version and ports"
@@ -7,9 +8,19 @@ apply_custom_ship_configs(){
     ## Applying version name
     sed -e "s/\(\"version\":\) .*$/\1 \"$(devel_version_and_id)\",/g" -i $config_json
 
-    ## Applying mobile_port
-
-    ## Applying web_port
+    ## Applying ports
+    local i
+    for i in $(seq 0 $((${#ALL_SITES[*]} - 1)))
+    do
+	if [ "$site" == "${ALL_SITES[$i]}" ]; then
+	    local mobile_port=$(echo $(($MOBILE_PORT_START + ${ALL_PORTS[$i]})))
+	    local web_port=$(echo $(($WEB_PORT_START + ${ALL_PORTS[$i]})))
+	    sed -e "s/\(\"ionic_port\":\) .*$/\1 \"$DEFAULT_MOBILE_PORT\",/g" -i $config_json
+	    sed -e "s/\(\"mobile_port\":\) .*$/\1 \"$mobile_port\",/g" -i $config_json
+	    sed -e "s/\(\"web_port\":\) .*$/\1 \"$web_port\",/g" -i $config_json
+	fi
+    done
+    
 
     ## Changing firefox_profile location in config.json
     if [ ! -z "$(ls $PAYLOADS_DIR/firefox)" ]; then
