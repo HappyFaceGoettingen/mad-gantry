0c9141fa9303a3b74a317e41d8bb5c349ef78b3b
GenKawamura <gen.kawamura@cern.ch>
2018-06-13 11:46:13 +0200

    Added a routine of swap configuraiton

--- libs/basic_packages.sh
+++ libs/basic_packages.sh
@@ -11,13 +11,34 @@ install_basic_packages(){
     sudo chkconfig docker on
     sudo usermod -aG dockerroot $USER
 
-    ## Docker storage configuraiton
-    [ ! -z "$DOCKER_STORAGE_SIZE" ] && sudo sed -e "s/^DOCKER_STORAGE_OPTIONS=.*$/DOCKER_STORAGE_OPTIONS=\"--storage-opt dm.basesize=$DOCKER_STORAGE_SIZE\"/g" -i /etc/sysconfig/docker-storage
-
     ## A useful alias to enable the docker socket    
     if [ ! -e /etc/profile.d/docker.sh ]; then
 	echo "alias docker-socket-on=\"sudo chmod 666 /var/run/docker.sock\"" > /tmp/docker.sh
 	sudo cp -v /tmp/docker.sh /etc/profile.d/
 	sudo chmod 666 /var/run/docker.sock
+    fi
+}
+
+
+make_swap(){
+    echo -n "Configure Swap? [y/n]> "
+    read proceed
+    [ "$proceed" != "y" ] && return 0
+
+    [ ! -e /root/swap ] && sudo mkdir -vp /root/swap
+    swap_file=/root/swap/swapfile
+    echo -n "Input size of swap space [GB] > "
+    read swap_size
+    
+    echo "swap size = ${swap_size} GB"
+    sudo dd if=/dev/zero of=$swap_file bs=100M count=$(($swap_size * 10))
+    
+    echo "adding swap space [$swap_file]"
+    sudo chmod 600 $swap_file
+    sudo mkswap $swap_file
+    sudo swapon $swap_file
+    
+    if [ -z "$(grep $swap_file /etc/fstab)" ]; then
+	echo "$swap_file               swap                    swap    defaults        0 0"
     fi
 }
