cc7876afcffbf29f3e2fe194ff7bf66c20fe95ad
GenKawamura <gen.kawamura@cern.ch>
2018-06-25 15:26:39 +0200

    Fixed a bug in ApplicationBuilder image

--- libs/level3.sh
+++ libs/level3.sh
@@ -1,10 +1,12 @@
 ## This function is generating templates of "site" level containers into ship & payloads
 put_level3_containers_onto_ship(){
     local site=$1
+    local default_ship_image=$SHIP_IMAGE
 
     local i
     for i in $(seq 0 $((${#ALL_LEVEL3_SITES[*]} - 1)))
     do
+	SHIP_IMAGE=$default_ship_image
 	if [ "${ALL_LEVEL3_SITES[$i]}" == "$site" ]; then
 	    [ ! -z "${ALL_LEVEL3_IMAGES[$i]}" ] && SHIP_IMAGE="${ALL_LEVEL3_IMAGES[$i]}"
 	    local site_dir=$PAYLOADS_DIR/sites/${ALL_LEVEL3_SITES[$i]}
