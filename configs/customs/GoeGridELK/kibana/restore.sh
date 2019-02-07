5f946232417e1a7b4f71ac67610e251e704ddaad
GenKawamura <gen.kawamura@cern.ch>
2019-02-07 14:56:44 +0100

    Kibana dashbord will be automatically recovered. Updated kibana/ES version

--- configs/customs/GoeGridELK/kibana/restore.sh
+++ configs/customs/GoeGridELK/kibana/restore.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd $(dirname $0)
-kibana_port=20261
+kibana_port=$1
+[ -z "$kibana_port" ] && kibana_port=5601
 curl -u elastic:changeme -k -XPOST "http://localhost:${kibana_port}/api/kibana/dashboards/import" -H 'Content-Type: application/json' -H "kbn-xsrf: true" -d @export.json
