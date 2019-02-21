fb59b32195e629ecd5b3b1997101b93b0cb0644a
GenKawamura <gen.kawamura@cern.ch>
2019-02-21 14:40:08 +0100

    geo_point and coordinate plot work

--- configs/customs/GoeGridELK/kibana/restore.sh
+++ configs/customs/GoeGridELK/kibana/restore.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 cd $(dirname $0)
+
+
+## For Kibana
 kibana_port=$1
 [ -z "$kibana_port" ] && kibana_port=5601
-curl -u elastic:changeme -k -XPOST "http://localhost:${kibana_port}/api/kibana/dashboards/import" -H 'Content-Type: application/json' -H "kbn-xsrf: true" -d @export.json
+if [ ! -e /tmp/kibana_dashboard.$UID ]; then
+    curl -u elastic:changeme -k -XPOST "http://localhost:${kibana_port}/api/kibana/dashboards/import" -H 'Content-Type: application/json' -H "kbn-xsrf: true" -d @export.json
+    [ $? -ne 0 ] && exit -1
+    date > /tmp/kibana_dashboard.$UID
+fi
+
+## For Elasticsearch
+elastic_url=$(grep elasticsearch.url /usr/share/kibana/config/kibana.yml | awk '{print $2}')
+[ -z "$elastic_url" ] && echo "elastic_url is null" && exit -1
+curl -XPUT "${elastic_url}/_template/dcache-billing" -H 'Content-Type: application/json' -d @mapping.json
+[ $? -ne 0 ] && "Failed: making template in [$elastic_url]" && exit -1
+
+echo "Success: making template in [$elastic_url]"
+exit 0
